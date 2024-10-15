b26700040b3e445a76cf395a6c28b912b6d90bbd
Діма Шимко <dimashimko7@gmail.com>
2024-10-15 10:07:54 +0300

    added statistics items to dashboard_page (beta)

---
+++ lib/pages/dashboard_page/widgets/statistics.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'miles_statistic.dart';
+
+class Statistics extends StatelessWidget {
+  const Statistics({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Wrap(
+      runSpacing: 12.0,
+      spacing: 12.0,
+      alignment: WrapAlignment.spaceBetween,
+      children: [
+        MilesStatistic(),
+        MilesStatistic(),
+      ],
+    );
+  }
+}
